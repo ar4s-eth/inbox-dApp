bd56a921587bef0bb02a37bd98c61478eacfd3dc
Ashley Barr <ashley.barr@ar4s.com>
2021-03-23 00:28:19 -0600

    add change message test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,15 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const initialString = 'Hello World'
 
 beforeEach(async () => {
   // Get a list of accounts
@@ -14,37 +18,25 @@ beforeEach(async () => {
   // Use one of those accounts to deploy
   // a contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hello World'] })
-    .send({ from: accounts[0], gas: '1000000' })
+    .deploy({ data: bytecode, arguments: [initialString] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('Should deploy a contract', () => {
     assert.ok(inbox.options.address);
   });
+
+  it(`Should have a default message ${initialString}`, async () => {
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, initialString);
+  });
+
+  it(`Should change the message`, async () => {
+    await inbox.methods.setMessage('Peaches').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, 'Peaches');
+  })
 });
-
-// class Car {
-//   park() {
-//     return 'stopped';
-//   }
-
-//   drive() {
-//     return 'vroom';
-//   }
-// }
-
-// let car;
-
-// beforeEach(() => {
-//   car = new Car();
-// });
-
-// describe('Car', () => {
-//   it(`Should return 'stopped'`, () => {
-//     assert.strictEqual(car.park(), 'stopped');
-//   });
-//   it(`Should return 'vroom'`, () => {
-//     assert.strictEqual(car.drive(), 'vroom');
-//   });
-// })
