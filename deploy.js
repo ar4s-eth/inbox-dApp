c4e50f86e219c5eda081d7b855999fbda9372e19
Ashley Barr <ashley.barr@ar4s.com>
2021-03-23 21:36:25 -0600

    add env seed/infura key

--- deploy.js
+++ deploy.js
@@ -1,7 +1,16 @@
-const HDWalletProvider = require('tuffle-hdwallet-provider');
+// Load private environmental variables into process.env
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
+// Assign ENV variables
+const privateSeed = process.env.SEED_PHRASE
+const ropsten = process.env.INFURA_ROPSTEN_KEY
+
+// Create provider
 const provider = new HDWalletProvider(
-  
-)
+  privateSeed, ropsten
+);
+
+const web3 = new Web3(provider);
